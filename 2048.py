c6b47acf840d927f0232ebd4ca10e990fd8db7f9
Norbert Benkó <rongybaba12@gmail.com>
2019-06-16 22:28:00 +0200

    mb last verison

--- 2048.py
+++ 2048.py
@@ -98,17 +98,17 @@ def control(board):
         board.clear()
         for x in rotated:
             board.append(x[::-1])
-    i = 0
+    y = 0
     for x in board:
         if any(y == 2048 for y in x):
             print("\033[91mYOU REACHED 2048! GGWP!\033[00m")
             sys.exit()
         if any(y == 0 for y in x):
             break
-        elif i == 3:
+        elif y == 3:
             print("\033[91mGAME OVER\033[00m")
             sys.exit()
-        i += 1
+        y += 1
     if can_random == True:
         random_2(board)
 
@@ -116,70 +116,34 @@ def control(board):
 def moves(rotated):
     can_random = False
     for x in range(4):
-        first_index = rotated[x][0]
-        second_index = rotated[x][1]
-        third_index = rotated[x][2]
-        fourth_index = rotated[x][3]
         observer = [True, True]
-        for y in range(1, 4):
+        for y in range(1,4):
             if rotated[x][y] == 0:
                 continue
-            else:
-                if y == 1 and first_index == 0:
-                    first_index += second_index
-                    second_index = 0
+            if rotated[x][0] == 0 or rotated[x][0] == rotated[x][y] and observer[0] == True:
+                if rotated[x][0] == 0:
+                    rotated[x][0] += rotated[x][y]
+                    rotated[x][y] = 0
                     can_random = True
-                elif y == 1 and first_index == second_index:
-                    first_index += second_index
-                    second_index = 0
+                else:
+                    rotated[x][0] += rotated[x][y]
+                    rotated[x][y] = 0
                     observer[0] = False
                     can_random = True
-                elif y == 2 and second_index == 0 and first_index == 0:
-                    first_index += third_index
-                    third_index = 0
+            elif y > 1 and rotated[x][1] == 0 or rotated[x][1] == rotated[x][y] and observer[1] == True and y > 1:
+                if rotated[x][1] == 0:
+                    rotated[x][1] += rotated[x][y]
+                    rotated[x][y] = 0
                     can_random = True
-                elif y == 2 and second_index == 0 and first_index == third_index and observer[0] == True:
-                    first_index += third_index
-                    third_index = 0
-                    observer[0] = False
-                    can_random = True
-                elif y == 2 and second_index == 0:
-                    second_index += third_index
-                    third_index = 0
-                    can_random = True
-                elif y == 2 and second_index == third_index:
-                    second_index += third_index
-                    third_index = 0
+                else:
+                    rotated[x][1] += rotated[x][y]
+                    rotated[x][y] = 0
                     observer[1] = False
                     can_random = True
-                elif y == 3 and third_index == 0 and second_index == 0 and first_index == 0:
-                    first_index += fourth_index
-                    fourth_index = 0
-                    can_random = True
-                elif y == 3 and third_index == 0 and second_index == 0 and first_index == fourth_index and observer[0] == True:
-                    first_index += fourth_index
-                    fourth_index = 0
-                    can_random = True
-                elif y == 3 and third_index == 0 and second_index == 0:
-                    second_index += fourth_index
-                    fourth_index = 0
-                    can_random = True
-                elif y == 3 and third_index == 0 and second_index == fourth_index and observer[1] == True:
-                    second_index += fourth_index
-                    fourth_index = 0
-                    can_random = True
-                elif y == 3 and third_index == 0:
-                    third_index += fourth_index
-                    fourth_index = 0
-                    can_random = True
-                elif y == 3 and third_index == fourth_index:
-                    third_index += fourth_index
-                    fourth_index = 0
-                    can_random = True
-        rotated[x][0] = first_index 
-        rotated[x][1] = second_index
-        rotated[x][2] = third_index
-        rotated[x][3] = fourth_index
+            elif y > 2 and rotated[x][2] == 0 or rotated[x][2] == rotated[x][y] and y > 2:
+                rotated[x][2] += rotated[x][y]
+                rotated[x][y] = 0
+                can_random = True
     return  can_random
 
 
--- test.py
+++ test.py
@@ -1,19 +1,70 @@
-board = [[1, 2, 3, 4], 
-         [5, 6, 7 ,8], 
-         [9, 10, 11, 12],
-         [13, 14, 15, 16]
-         ]
-print(board)
-rotated = []
-temp = list(zip(*board[::-1]))
-for x in temp:
-    rotated.append(list(x))
-board.clear()
-print(rotated)
-temp.clear()
-temp = list(zip(*rotated))
-temp = temp[::-1]
-for x in temp:
-    board.append(list(x))
-print(board)
+def moves(rotated):
+    can_random = False
+    for x in range(4):
+        first_index = rotated[x][0]
+        second_index = rotated[x][1]
+        third_index = rotated[x][2]
+        fourth_index = rotated[x][3]
+        observer = [True, True]
+        for y in range(1, 4):
+            if rotated[x][y] == 0:
+                continue
+            else:
+                if y == 1 and first_index == 0:
+                    first_index += second_index
+                    second_index = 0
+                    can_random = True
+                elif y == 1 and first_index == second_index:
+                    first_index += second_index
+                    second_index = 0
+                    observer[0] = False
+                    can_random = True
+                elif y == 2 and second_index == 0 and first_index == 0:
+                    first_index += third_index
+                    third_index = 0
+                    can_random = True
+                elif y == 2 and second_index == 0 and first_index == third_index and observer[0] == True:
+                    first_index += third_index
+                    third_index = 0
+                    observer[0] = False
+                    can_random = True
+                elif y == 2 and second_index == 0:
+                    second_index += third_index
+                    third_index = 0
+                    can_random = True
+                elif y == 2 and second_index == third_index:
+                    second_index += third_index
+                    third_index = 0
+                    observer[1] = False
+                    can_random = True
+                elif y == 3 and third_index == 0 and second_index == 0 and first_index == 0:
+                    first_index += fourth_index
+                    fourth_index = 0
+                    can_random = True
+                elif y == 3 and third_index == 0 and second_index == 0 and first_index == fourth_index and observer[0] == True:
+                    first_index += fourth_index
+                    fourth_index = 0
+                    can_random = True
+                elif y == 3 and third_index == 0 and second_index == 0:
+                    second_index += fourth_index
+                    fourth_index = 0
+                    can_random = True
+                elif y == 3 and third_index == 0 and second_index == fourth_index and observer[1] == True:
+                    second_index += fourth_index
+                    fourth_index = 0
+                    can_random = True
+                elif y == 3 and third_index == 0:
+                    third_index += fourth_index
+                    fourth_index = 0
+                    can_random = True
+                elif y == 3 and third_index == fourth_index:
+                    third_index += fourth_index
+                    fourth_index = 0
+                    can_random = True
+        rotated[x][0] = first_index 
+        rotated[x][1] = second_index
+        rotated[x][2] = third_index
+        rotated[x][3] = fourth_index
+    return  can_random
+
 
